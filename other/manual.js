0be1f6b629776d6f93306c0e11ee89d4888c381c
fbslo <admin@fbslo.net>
2020-08-04 15:10:58 +0200

    more manual registrations

--- other/manual.js
+++ other/manual.js
@@ -3,7 +3,7 @@ const con = require('../database.js')
 const config = require("../config.json")
 
 
-var list = [['cadawg', "_CADawg"], ["sbaby", "sbaby48002048"], ["toofasteddie", "thecryptorunner"], ["franzzonline", "Franzzonline"], ["plantstoplanks", "PlantsToPlanks"], ["hatoto", "kaesetopf"], ["zoeanavid", "ZoeAnavidV"], ["fionasfavourites", "FionasFavourite"]]
+var list = [['tykee', "realmikes_tykee"], ["anwars", "anwars14441962"], ["adncabrera", "adncabrera1"], ["andy4475", "Social_Hiver"], ["gniksivart", "GnikSivart"], ["fabiyamada", "mxYamada"], ["jauregui98", "gjauregui98"], ["mister-meeseeks", "MisterMeeseeks3"]]
 
 start(0, list)
 
