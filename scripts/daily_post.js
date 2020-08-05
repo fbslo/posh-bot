18161ab9336da798f79b4199db48f6c00a7abce0
fbslo <admin@fbslo.net>
2020-08-05 19:12:12 +0200

    fix daily posts

--- scripts/daily_post.js
+++ scripts/daily_post.js
@@ -4,17 +4,17 @@ const hive = require('@hiveio/hive-js')
 
 
 function post(){
-  let one_day = new Date().getTime() - 864000000
+  let one_day = new Date().getTime() - 86400000
   let now = new Date().getTime()
   con.query(`SELECT hive_username, user_name, SUM(points) AS sum FROM twitter_posts WHERE points_time BETWEEN ${one_day} AND ${now} GROUP BY hive_username, user_name ORDER BY sum DESC;`, (err, result) => { //points_time <= ${one_day}
     if(err) console.log("Error with database: Error: "+err)
     else {
-      submitHivePost(result)
+      submitHivePost(result, one_day, now)
     }
   })
 }
 
-async function submitHivePost(data){
+async function submitHivePost(data, one_day, now){
   var today = new Date();
   var dd = String(today.getDate()).padStart(2, '0');
   var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
@@ -27,13 +27,13 @@ async function submitHivePost(data){
   let permlink = makeid(15).toLowerCase()
   if(data.length == 0){
     console.log('empty')
-    hive.broadcast.comment(config.posting_key, '', 'posh', config.account_name, permlink, 'Daily #POSH stats! '+today, 'No new #POSH tweets received tokens today :(', jsonMetadata, function(err, result) {
+    hive.broadcast.comment(config.posting_key, '', 'posh', config.account_name, permlink, 'Daily #POSH stats! '+today, `No new #POSH tweets received tokens today :(\nTweets that were created between ${new Date(one_day)} and ${new Date(one_day)}`, jsonMetadata, function(err, result) {
       if(err) console.log('Daily post failed! Err: '+err)
       else console.log('Daily (EMPTY) post submited!')
     });
   } else {
-    richlist(async (rich) => {
-      let body = await prepareBody(data)
+    richlist(async (rich, one_day, now) => {
+      let body = await prepareBody(data, one_day, now)
       body += `\n\n<center><h3>Top 50 earners</h3></center>\n\n|Hive username|Twitter username|Tokens earned|\n|---|---|---|\n`+rich
       hive.broadcast.comment(config.posting_key, '', 'posh', config.account_name, permlink, 'Daily #POSH stats! '+today, body, jsonMetadata, function(err, result) {
         if(err) console.log('Daily post failed! Err: '+err)
@@ -61,8 +61,8 @@ async function submitHivePost(data){
   }
 }
 
-function prepareBody(data){
-  let body = '<center><h3>Total number of tokens distributed today: 1,000</h3></center>\n\n|Hive username|Twitter username|Tokens earned today|\n|---|---|---|\n'
+function prepareBody(data, one_day, now){
+  let body = `<center><h3>Total number of tokens distributed today: 1,000</h3></center>\nTweets that were created between ${new Date(one_day)} and ${new Date(one_day)}\n\n|Hive username|Twitter username|Tokens earned today|\n|---|---|---|\n`
   for (i in data){
     body += `|@${data[i].hive_username}|${data[i].user_name}|${data[i].sum}|\n`
     updatePost(data[i].id)
