ca9095fc7dcc6d03b6de5317ffa4e033e43a2581
fbslo <admin@fbslo.net>
2020-10-18 19:42:57 +0200

    fix new Date().getTime()

--- scripts/calculateEngagementScore.js
+++ scripts/calculateEngagementScore.js
@@ -14,7 +14,7 @@ var T = new Twit({
 })
 
 function calculate(){
-  database.findOne({timestamp: {$gt: new Date().getTime - 86400000}, engagementScore: 'NULL'}, async (err, result) => { //find all tweets older than one day
+  database.findOne({timestamp: {$gt: new Date().getTime() - 86400000}, engagementScore: 'NULL'}, async (err, result) => { //find all tweets older than one day
     if (err) console.log(`Error calculating engagementScore! Database error: ${err}`)
     else {
       if (result == null) console.log(`No new tweets to calculate engagementScore for.`)
--- scripts/calculateTokens.js
+++ scripts/calculateTokens.js
@@ -3,7 +3,7 @@ const database = mongo.get().db("Posh").collection("tweets")
 
 function calculate(){
   return new Promise((resolve, reject) => {
-    database.findOne({engagementTime: {$gt: new Date().getTime - 86400000}, tokens: 'NULL'}, async (err, result) => { //find all tweets that received engagementScore in last day
+    database.findOne({engagementTime: {$gt: new Date().getTime() - 86400000}, tokens: 'NULL'}, async (err, result) => { //find all tweets that received engagementScore in last day
       if (err) console.log(`Error calculating tokens! Database error: ${err}`)
       else {
         if (result == null) console.log(`No new tweets to calculate tokens for.`)
